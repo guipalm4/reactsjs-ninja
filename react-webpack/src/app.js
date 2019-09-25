d6d8930d6578008d0c235ffe6e028294dd748107
Guilherme Gomes Palma <guilherme.palma@dbserver.com.br>
2019-09-25 15:47:42 -0300

    MRW#05 - Criando uma aplicação em React

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -1,6 +1,10 @@
 'use strinct'
 
-function sum (val1, val2){
-    return val1 + val2
-}
-module.exports = sum
+var React = require('react')
+
+var Title = React.createClass({
+    render: function () {
+        return React.createElement('h1', null, 'Titulo')
+    }
+})
+module.exports = Title
