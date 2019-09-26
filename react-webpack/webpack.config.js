2766d9d61dd4a6548b7449ca7781429b52d02012
Guilherme Gomes Palma <guilherme.palma@dbserver.com.br>
2019-09-26 12:16:44 -0300

    MRW#10 - Configurando a ferramenta de lint

--- react-webpack/webpack.config.js
+++ react-webpack/webpack.config.js
@@ -2,25 +2,36 @@
 
 const path = require('path')
 const webpack = require ('webpack')
+const validate = require('webpack-validator')
 
-module.exports = {
+module.exports = validate({
     devtool: 'source-map',  // Facilita debug aplicacao
+    
     entry: [
         'react-hot-loader/patch',
         'webpack-dev-server/client?http://localhost:3000',
         'webpack/hot/only-dev-server',        
         path.join(__dirname, 'src', 'index')
     ],
+
     output:{
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/static'
+        publicPath: '/dist/'
     },
 
     plugins:[
         new webpack.HotModuleReplacementPlugin()
     ],
+
     module:{
+        preLoaders: [{
+            test:/\.js$/,
+            exclude: /node_modules/,
+            include: /src/,
+            loader: 'standard'
+        }],
+
         loaders: [{
             test:/\.js$/,
             exclude: /node_modules/,
@@ -28,4 +39,4 @@ module.exports = {
             loader: 'babel'
         }]
     }
-}
+})
