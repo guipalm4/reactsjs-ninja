cc953541e82d38fb7f5c0c99ff625030d31c528c
Guilherme Gomes Palma <guilherme.palma@dbserver.com.br>
2019-10-01 10:03:44 -0300

    M1#A16 - A prop children

--- m01/01-learning-props/src/app.js
+++ m01/01-learning-props/src/app.js
@@ -3,15 +3,16 @@
 import React, { Component } from 'react'
 import Title from './title'
 import Square from './square'
+import Button from './button'
 
 class App extends Component {
   render () {
     return (
       <div className='container'>
-        <Title name='Guilherme' lastname='Palma' />
-        {['blue', 'red', 'blue'].map((square, index) => (
-          <Square key={index} color={square} />
-        ))}
+        <Button>Texto children
+          <br/>
+          <span>Guilherme</span>
+        </Button>
       </div>
     )
   }
