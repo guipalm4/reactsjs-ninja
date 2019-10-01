2ede80f8e82061ee7f44463ec006e259180864ce
Guilherme Gomes Palma <guilherme.palma@dbserver.com.br>
2019-10-01 10:31:08 -0300

    M1#A17 - Composição

--- m01/01-learning-props/src/app.js
+++ m01/01-learning-props/src/app.js
@@ -4,15 +4,15 @@ import React, { Component } from 'react'
 import Title from './title'
 import Square from './square'
 import Button from './button'
+import LikeButton from './like-button'
+import SearchButton from './search-button'
 
 class App extends Component {
   render () {
     return (
       <div className='container'>
-        <Button>Texto children
-          <br/>
-          <span>Guilherme</span>
-        </Button>
+        <LikeButton />
+        <SearchButton />
       </div>
     )
   }
---
+++ m01/01-learning-props/src/search-button.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import React from 'react'
+import Button from './Button'
+
+
+const SearchButton = () => (
+  <Button handleClick={() => alert('search')}>
+    Search
+  </Button>
+)
+
+export default SearchButton
