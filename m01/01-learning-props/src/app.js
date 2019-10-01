30c84411a07a72cf848d285fa4dd2ec3de4f3d68
Guilherme Gomes Palma <guilherme.palma@dbserver.com.br>
2019-10-01 14:31:00 -0300

    M1#A18 - State

--- m01/01-learning-props/src/app.js
+++ m01/01-learning-props/src/app.js
@@ -8,11 +8,20 @@ import LikeButton from './like-button'
 import SearchButton from './search-button'
 
 class App extends Component {
+
+  constructor () {
+      super()
+      this.state = {
+        text: 'Guilherme'
+      }
+    }
+    
   render () {
     return (
-      <div className='container'>
-        <LikeButton />
-        <SearchButton />
+      <div className='container' onClick={() => this.setState({
+          text: 'Outro texto'
+        })}>
+          {this.state.text}
       </div>
     )
   }
