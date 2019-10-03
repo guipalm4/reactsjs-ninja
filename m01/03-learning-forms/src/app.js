0490cf2b2a9056eb6e73f8a0464c37862b5f2f39
Guilherme Gomes Palma <guilherme.palma@dbserver.com.br>
2019-10-03 13:25:38 -0300

    M1#A28 - Introdução à formulários no React

---
+++ m01/03-learning-forms/src/app.js
@@ -0,0 +1,29 @@
+'use strinct'
+
+import React, { Component } from 'react'
+
+class App extends Component {
+
+  constructor () {
+    super()
+    this.state = {
+      value: 'Valor incial'
+    }
+  }
+  
+  render (){
+    return (
+      <div>
+        <form>
+          <input type='text' value={this.state.value} onChange={(e) => {            
+            this.setState({
+              value: e.target.value
+            })
+          }} />
+        </form>
+      </div>
+    )
+  }  
+}
+
+export default App
