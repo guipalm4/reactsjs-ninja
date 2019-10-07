d62f3b432e78f7c7c6bc65163537973b2742c0fb
Guilherme Palma <gomes.lmc@gmail.com>
2019-10-06 23:20:41 -0300

    M1#A35 - GitHub app - Criando a marcação da aplicação

--- m01/github-app/src/app.js
+++ m01/github-app/src/app.js
@@ -3,8 +3,45 @@
 import React from 'react'
 
 const App = () => (  
-  <div>
-    My App
+  <div className='app'>
+
+  <div className='search'>
+    <imput 
+      type='search'
+      placeholder='Digite o nome do usuário no GitHub'/>
+  </div>
+
+    <div className='user_info'>
+      <img src='https://avatars0.githubusercontent.com/u/18333576?v=4' />
+      <h2>
+        <a href='https://api.github.com/users/Zarppa'>Guilherme Palma</a>
+      </h2> 
+
+      <ul className='repos-info'>
+        <li>- Repositórios: 12</li>
+        <li>- Seguidores: 12</li>
+        <li>- Seguindo: 3</li>
+      </ul> 
+      
+      <div className='actions'>
+        <button> Ver repositórios</button> 
+        <button> Ver favoritos</button> 
+      </div>
+
+      <div className='repos'>
+        <h2>Repositórios:</h2>
+        <ul>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+
+      <div className='starred'>
+        <h2>Favoritos:</h2>
+        <ul>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+    </div>
   </div>
 )
 export default App
