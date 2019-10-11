3fcdad55256e20636ff52ded9619987ab483daf0
Guilherme Gomes Palma <guilherme.palma@dbserver.com.br>
2019-10-11 14:38:59 -0300

    M2#A06 - Jest na prática - code coverage

--- m02/jest-in-practice/sum.test.js
+++ m02/jest-in-practice/sum.test.js
@@ -1,9 +1,9 @@
 'use strict'
 
-it('Um é igual a 1', () => {
-  console.assert(1 === 2 , '1 não é igual a 2')
+const expect = require('chai').expect
+const sum = require('./sum')
+
+it('sum should be a function', () => {
+  expect(sum).to.be.a('function')
 })
-
-
-
 
